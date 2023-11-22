399def27f30849c26a88958e32bd933e4e642254
eminbasbayan <emin.basbayan@icloud.com>
2023-11-22 23:14:02 +0300

    import export patterns

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// const superHero = require("./super-hero.js")
-
-// console.log(superHero.getName());
-
-// superHero.setName("Superman")
-// console.log(superHero.getName());
-
-// const newSuperHero = require("./super-hero.js");
-// console.log(newSuperHero.getName());
-const SuperHero = require("./super-hero");
-const superman = new SuperHero("Superman")
-console.log(superman.getName());
-const batman = new SuperHero("Batman")
-console.log(batman.getName());
+const math = require("./math.js");
+const { add, subtract } = math;
+console.log(add(5, 7));
+console.log(subtract(5, 1));
---
+++ math.js
@@ -0,0 +1,9 @@
+exports.add = (a, b) => {
+  return a + b;
+};
+
+exports.subtract = (a, b) => {
+  return a - b;
+};
+
+

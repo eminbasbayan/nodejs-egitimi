4885dcf3bc9722e459dbd8b4051fb2970cde357a
eminbasbayan <emin.basbayan@icloud.com>
2023-11-22 22:08:17 +0300

    module caching

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require("./batman.js");
-require("./superman.js");
+// const superHero = require("./super-hero.js")
+
+// console.log(superHero.getName());
+
+// superHero.setName("Superman")
+// console.log(superHero.getName());
+
+// const newSuperHero = require("./super-hero.js");
+// console.log(newSuperHero.getName());
+const SuperHero = require("./super-hero");
+const superman = new SuperHero("Superman")
+console.log(superman.getName());
+const batman = new SuperHero("Batman")
+console.log(batman.getName());

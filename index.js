cb23a080056996f3bf3910defbd5bb85d43ada2f
eminbasbayan <emin.basbayan@icloud.com>
2023-11-25 14:26:02 +0300

    importing JSON and watch mode

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const math = require("./math.js");
-const { add, subtract } = math;
-console.log(add(5, 7));
-console.log(subtract(5, 1));
+const data = require("./data.json");
+
+console.log(data);

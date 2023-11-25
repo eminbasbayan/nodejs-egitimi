03a9ead676e128897e6b18838166b97569fcfdc2
eminbasbayan <emin.basbayan@icloud.com>
2023-11-25 20:54:47 +0300

    callback pattern

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const path = require("node:path");
+function hello(name) {
+  console.log(`Hello, ${name}`);
+}
 
-// console.log(__filename);
-// console.log(__dirname);
+function helloName(helloFn) {
+  const name = "Nur";
+  helloFn(name);
+}
 
-// console.log(path.basename(__filename));
-// console.log(path.basename(__dirname));
-
-// console.log(path.extname(__filename));
-// console.log(path.extname(__dirname));
-
-// console.log(path.parse(__filename));
-// console.log(path.format(path.parse(__filename)));
-
-// console.log(path.isAbsolute(__filename));
-// console.log(path.isAbsolute("add.js"));
-
-console.log(__filename);
-// console.log(path.join("folder1", "folder2", "index.html"));
-// console.log(path.join("/folder1", "folder2", "index.html"));
-// console.log(path.join("/folder1", "//folder2", "index.html"));
-// console.log(path.join("/folder1", "//folder2", "../index.html"));
-
-console.log(path.resolve("folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "index.html"));
-console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+helloName(hello);

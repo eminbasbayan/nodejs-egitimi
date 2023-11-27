4c9ef422905ab7fb598cf16cff201f04a4df1199
eminbasbayan <emin.basbayan@icloud.com>
2023-11-27 22:51:52 +0300

    fs module

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const buffer = new Buffer("BilGen");
+const fs = require("node:fs");
 
-buffer.write("Emin");
+//! Senkron dosya okuma
+// const txtFile = fs.readFileSync("./file.txt", "utf-8");
+// console.log(txtFile.toString());
+// console.log("sync");
+//! Asenkron dosya okuma
+// fs.readFile("./file.txt", (err, data) => {
+//   setTimeout(() => {
+//     if (err) {
+//       console.log("Dosya okunurken bir hata oluştu:", err);
+//     } else {
+//       console.log(data.toString());
+//     }
+//   }, 3000);
+// });
+// console.log("async");
 
-console.log(buffer.toString());
-console.log(buffer);
-console.log(buffer.toJSON());
+//! Dosya içi yazıyı senkron değiştirme
+// fs.writeFileSync("./file.txt", "Hello World!");
+
+//! Dosya içi yazıyı asenkron değiştirme
+// fs.writeFile("./file.txt", "Emin Başbayan", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Dosya başarıyla yazıldı!");
+//   }
+// });
+
+//! Dosya içi yazıyı asenkron append ile yazının sonuna istenilen yazıyı ekleme
+fs.writeFile("./file.txt", "Emin Başbayan", { flag: "a" }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Dosya başarıyla yazıldı!");
+  }
+});

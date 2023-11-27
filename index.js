f262e2319fc06e00e59e60b18f34cc2720e8e51e
eminbasbayan <emin.basbayan@icloud.com>
2023-11-27 12:01:59 +0300

    streams and buffers

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-const PizzaShop = require("./pizza-shop");
-const DrinkMachine = require("./drink-machine");
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
+const buffer = new Buffer("BilGen");
 
-pizzaShop.on("order", (size, topping) => {
-  console.log(`Sipariş alındı: ${size} pizza, ${topping} ile`);
-  drinkMachine.serveDrink(size);
-});
+buffer.write("Emin");
 
-pizzaShop.order("large", "sucuk");
-pizzaShop.displayOrderNumber();
-
-// const EventEmitter = require("events");
-// const emitter = new EventEmitter();
-
-// emitter.on("orderPizza", (size, topping) => {
-//   console.log(`Bir ${size} pizza pişiriliyor. İçindekiler: ${topping}`);
-// });
-
-// emitter.on("orderPizza", (size) => {
-//   if (size === "large") {
-//     console.log(`Ücretsiz içecek servisi.`);
-//   }
-// });
-
-// emitter.emit("orderPizza", "small", "mantar");
+console.log(buffer.toString());
+console.log(buffer);
+console.log(buffer.toJSON());

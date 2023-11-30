6446d59104f46f569bcbd6741bc39a941d6eaed2
eminbasbayan <emin.basbayan@icloud.com>
2023-11-30 15:40:09 +0300

    creating a node server

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const fs = require("node:fs");
-const zlib = require("node:zlib");
+const http = require("node:http");
 
-const gzip = zlib.createGzip();
-
-const readableStream = fs.createReadStream("./file.txt", {
-  encoding: "utf-8",
-  highWaterMark: 2,
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World!");
 });
 
-readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"));
-
-// const writeableStream = fs.createWriteStream("./file2.txt");
-
-// readableStream.pipe(writeableStream);
-
-// readableStream.on("data", (chunk) => {
-//   console.log(chunk);
-//   writeableStream.write(chunk);
-// });
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});

f541bf520e2284c506a8bd443cd44f7a07cd43f0
eminbasbayan <emin.basbayan@icloud.com>
2023-12-01 15:23:21 +0300

    JSON response

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require("node:http");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello World!");
+  const instructor = {
+    firstName: "Emin",
+    lastName: "Başbayan",
+  };
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(instructor));
 });
 
 server.listen(3000, () => {

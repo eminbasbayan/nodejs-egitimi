fa6adcf06ee3c32a2f04c2c04cf7a9606c43c5a4
eminbasbayan <emin.basbayan@icloud.com>
2023-12-01 22:39:11 +0300

    http routing

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const http = require("node:http");
 const fs = require("node:fs");
 
-const server = http.createServer((request, response) => {
-  const name = "Emin";
-
-  response.writeHead(200, { "Content-Type": "text/html" });
-  // fs.createReadStream(__dirname + "/index.html").pipe(response);
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  response.end(html);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home Page");
+  } else if (req.url === "/products") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Products Page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Emin",
+        lastName: "Başbayan",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not found!");
+  }
 });
 
 server.listen(3000, () => {

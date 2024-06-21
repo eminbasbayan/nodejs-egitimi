d7533ebae194c6b94345c8990afa2808c393b86f
eminbasbayan <emin.basbayan@icloud.com>
2024-06-21 12:40:03 +0300

    check queue

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const fs = require('fs');
+setTimeout(() => console.log("Set Timeout Callback 1"), 0);
+setImmediate(() => console.log("Set Immediate Callback 1"));
 
-fs.readFile(__filename, (err, data) => {
-    console.log('Read File Callback');
-});
 
-process.nextTick(() => {
-    console.log('Next Tick Callback');
-});
+/* ****************** */
 
-Promise.resolve().then(() => {
-    console.log('Promise Callback');
-});
+// setImmediate(() => console.log("Set Immediate Callback 1"));
 
-setTimeout(() => {
-    console.log('Set Timeout Callback');
-}, 0);
+// setImmediate(() => {
+//   console.log("Set Immediate Callback 2");
+//   process.nextTick(() => {
+//     console.log("Set Immediate 2 içinde Next Tick Callback");
+//   });
 
-setImmediate(()=> {
-  console.log("Set Immediate Callback");
-})
+//   Promise.resolve().then(() => {
+//     console.log("Set Immediate 2 içinde Promise Callback");
+//   });
+// });
 
+// setImmediate(() => console.log("Set Immediate Callback 3"));
+
+/* ************* */
+
+
+// const fs = require('fs');
+
+// fs.readFile(__filename, () => {
+//     console.log('Read File Callback');
+
+//     setImmediate(() => console.log("ReadFile içinde Set Immediate Callback"));
+
+//     process.nextTick(() => {
+//       console.log("ReadFile içinde Next Tick Callback");
+//     });
+  
+//     Promise.resolve().then(() => {
+//       console.log("ReadFile içinde Promise Callback");
+//     });
+// });
+
+// process.nextTick(() => {
+//     console.log('Next Tick Callback');
+// });
+
+// Promise.resolve().then(() => {
+//     console.log('Promise Callback');
+// });
+
+// setTimeout(() => {
+//     console.log('Set Timeout Callback');
+// }, 0);
